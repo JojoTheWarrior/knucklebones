f29786e434a197507ada42959c2216d5d998d8fe
JojoTheWarrior <joshua.wang828@gmail.com>
2026-08-13 17:32:31 -0400

    stuck on cyclic states

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Consider for one column, the number of ways to fill it. The column has 3+3 slots
 
 Let $C$ be the number of ways to fill one column.
 
-We can use $C \leq \binom{8}{3}^2 = 56^2 = 3136$ as an upper bound for each column.
+We can use $C \leq \binom{9}{3}^2 = 84^2 = 7056$ as an upper bound for each column.
 
 Running `state_bounder.py`, we're getting the exact number of states to be $C = 3067$.
 
@@ -19,11 +19,22 @@ The calculation is $\binom{C-1+3}{3}=\binom{3069}{3}\approx4.8 \times 10^9$.
 
 If we can compress every game state to a single index, then we can store all $4.8$ billion states in around $625 MB$.
 
-If we store, for each state, simply the next state it should move to, assuming we store the next index as a $32$ bit number, this would cost around $20$ GB. Fuck.
+If we store, for each state, simply the next state it should move to, assuming we store the next index as a $32$ bit integer, this would cost around $20$ GB. Fuck.
+
+If we store, for each state, the probability that we win, assuming we store the probability as a $32$ bit float, this would still cost around $20$ GB.
 
 # Other ideas
+
+Are there any symmetrical board positions we can take advantage of to reduce the total number of states?
+
+Are there any states where you're guaranteed to lose?
+- Probably, but these are very limited because any move can technically get rid of your opponent's die and expand the game state
 
 Is there a way to analyze this game using Sprague-Grundy numbers?
 
 - Probably not, since the game is not impartial
 - And you can't really XOR the Nimbers of columns to determine whether the board is winning or losing
+
+Use RL to train models to play next moves.
+
+Try simulating a few moves in the future, and pick the one where you have the highest score.
--- state_bounder.py
+++ state_bounder.py
@@ -1,26 +1,32 @@
-from itertools import product
+from itertools import product, combinations_with_replacement
 
 # state is length 6, [0, 1, 2] is Top and [3, 4, 5] is Bottom, -1 means it's empty
 def check_valid(state: list) -> bool:
-    # check whether the state matches its sorted self, to avoid counting duplicates
-    sorted_state = state.copy()
-    sorted_state[0:3] = sorted(sorted_state[0:3])
-    sorted_state[3:6] = sorted(sorted_state[3:6])
+        # check whether the state matches its sorted self, to avoid counting duplicates
+        sorted_state = state.copy()
+        sorted_state[0:3] = sorted(sorted_state[0:3])
+        sorted_state[3:6] = sorted(sorted_state[3:6])
 
-    if not sorted_state == state:
-        return False
-
-    # check each number appears in either top or bottom
-    for i in range(1, 7):
-        inTop, inBottom = False, False
-        for j in range(3):
-            inTop |= state[j] == i
-            inBottom |= state[3+j] == i
-        if inTop and inBottom:
+        if not sorted_state == state:
             return False
-    return True
 
+        # check each number appears in either top or bottom
+        for i in range(1, 7):
+            inTop, inBottom = False, False
+            for j in range(3):
+                inTop |= state[j] == i
+                inBottom |= state[3+j] == i
+            if inTop and inBottom:
+                return False
+        return True
 
+class Column:
+    columns = [list(state) for state in product([-1] + list(range(1, 7)), repeat=6) if check_valid(list(state))]
+    rev_columns = {tuple(column) : i for i, column in enumerate(columns)} # note that lists are unhashable in python
+
+    print(rev_columns[(-1, -1, 1, 2, 3, 4)])
+
+# says C = 3067
 valid_counter = 0
 for state in product([-1, 1, 2, 3, 4, 5, 6], repeat=6):
     if check_valid(list(state)):
